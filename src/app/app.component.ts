879491eada4d2c9799817eb29868b1b2382e145a
Keenan stewart <keenanstewart1976@gmail.com>
2018-01-15 09:42:13 -0400

    Initial setup for reactive-forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, FormArray} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+    title = "reactive-forms demo";
+
+    loginForm: FormGroup = new FormGroup({
+        email: new FormControl(""),
+        password: new FormControl("")
+    });
+    
 }
